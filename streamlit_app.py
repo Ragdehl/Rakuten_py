ce46e36b3c730f7d22496f3742a433b659730e9c
Ragdehl <idalgo88@gmail.com>
2021-04-06 22:40:31 +0200

    Update streamlit_app.py
    New update!

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,9 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
