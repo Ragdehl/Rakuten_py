4d4743b8236e05292064fc20f58f0fa1ab72bfc4
emarchan-prog <82802595+emarchan-prog@users.noreply.github.com>
2021-04-20 18:25:32 +0200

    Prototype de streamlit intéractif
    Programme streamlit qui présente les données de façon intéractive.

---
+++ eric/rakuten_streamlit.bat
@@ -0,0 +1,6 @@
+@echo on
+call C:\Users\emarchan\Anaconda3\Scripts\activate.bat "ScientestProjet"
+call streamlit.exe run C:\Users\emarchan\Scientest\project_streamlit.py
+rem call conda list pandas
+pause
+
